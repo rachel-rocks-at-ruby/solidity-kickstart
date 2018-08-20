c19af8fb76b06d7f807c28a1b63da5e1e697e66a
rachel-rocks-at-ruby <rachel@voytravel.com>
2018-08-20 11:26:13 -0700

    installing next.js

---
+++ .next/dist/pages/index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _jsxFileName = '/Users/rachelbird/Code/Ethereum and Solidity Class Projects/kickstart/pages/index.js?entry';
+
+exports.default = function () {
+	return _react2.default.createElement('h1', {
+		__source: {
+			fileName: _jsxFileName,
+			lineNumber: 5
+		}
+	}, ' This is the campaign list page!!!');
+};
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzL2luZGV4LmpzIl0sIm5hbWVzIjpbIlJlYWN0Il0sIm1hcHBpbmdzIjoiOzs7Ozs7QUFBQSxBQUFPLEFBRVA7Ozs7Ozs7O2tCQUFlLFlBQU0sQUFDcEI7d0JBQ0MsY0FBQTs7YUFBQTtlQUFBO0FBQUE7QUFBQSxFQUFBLEVBREQsQUFDQyxBQUVEO0FBSkQiLCJmaWxlIjoiaW5kZXguanM/ZW50cnkiLCJzb3VyY2VSb290IjoiL1VzZXJzL3JhY2hlbGJpcmQvQ29kZS9FdGhlcmV1bSBhbmQgU29saWRpdHkgQ2xhc3MgUHJvamVjdHMva2lja3N0YXJ0In0=
