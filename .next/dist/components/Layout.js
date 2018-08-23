714f96bc8b3da780d54c6f2a9d57f87b344f8885
rachel-rocks-at-ruby <rachel@voytravel.com>
2018-08-22 17:07:03 -0700

    adding new campaigns page and custom routing

--- .next/dist/components/Layout.js
+++ .next/dist/components/Layout.js
@@ -10,6 +10,10 @@ var _react2 = _interopRequireDefault(_react);
 
 var _semanticUiReact = require('semantic-ui-react');
 
+var _head = require('next/dist/lib/head.js');
+
+var _head2 = _interopRequireDefault(_head);
+
 var _Header = require('./Header');
 
 var _Header2 = _interopRequireDefault(_Header);
@@ -22,13 +26,24 @@ exports.default = function (props) {
 	return _react2.default.createElement(_semanticUiReact.Container, {
 		__source: {
 			fileName: _jsxFileName,
-			lineNumber: 7
+			lineNumber: 8
 		}
-	}, _react2.default.createElement(_Header2.default, {
+	}, _react2.default.createElement(_head2.default, {
 		__source: {
 			fileName: _jsxFileName,
-			lineNumber: 8
+			lineNumber: 9
+		}
+	}, _react2.default.createElement('link', {
+		rel: 'stylesheet',
+		href: '//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.3/semantic.min.css', __source: {
+			fileName: _jsxFileName,
+			lineNumber: 10
+		}
+	})), _react2.default.createElement(_Header2.default, {
+		__source: {
+			fileName: _jsxFileName,
+			lineNumber: 15
 		}
 	}), props.children);
 };
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImNvbXBvbmVudHMvTGF5b3V0LmpzIl0sIm5hbWVzIjpbIlJlYWN0IiwiQ29udGFpbmVyIiwiSGVhZGVyIiwicHJvcHMiLCJjaGlsZHJlbiJdLCJtYXBwaW5ncyI6Ijs7Ozs7O0FBQUEsQUFBTzs7OztBQUNQLEFBQVM7O0FBQ1QsQUFBTyxBQUFZLEFBRW5COzs7Ozs7OztrQkFBZSxpQkFBUyxBQUN2Qjt3QkFDQyxBQUFDOzthQUFEO2VBQUEsQUFDQztBQUREO0FBQUEsRUFBQSxrQkFDQyxBQUFDOzthQUFEO2VBREQsQUFDQyxBQUNDO0FBREQ7QUFBQSxXQUZGLEFBQ0MsQUFFUSxBQUdUO0FBUEQiLCJmaWxlIjoiTGF5b3V0LmpzIiwic291cmNlUm9vdCI6Ii9Vc2Vycy9yYWNoZWxiaXJkL0NvZGUvRXRoZXJldW0gYW5kIFNvbGlkaXR5IENsYXNzIFByb2plY3RzL2tpY2tzdGFydCJ9
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImNvbXBvbmVudHMvTGF5b3V0LmpzIl0sIm5hbWVzIjpbIlJlYWN0IiwiQ29udGFpbmVyIiwiSGVhZCIsIkhlYWRlciIsInByb3BzIiwiY2hpbGRyZW4iXSwibWFwcGluZ3MiOiI7Ozs7OztBQUFBLEFBQU87Ozs7QUFDUCxBQUFTOztBQUNULEFBQU87Ozs7QUFDUCxBQUFPLEFBQVksQUFFbkI7Ozs7Ozs7O2tCQUFlLGlCQUFTLEFBQ3ZCO3dCQUNDLEFBQUM7O2FBQUQ7ZUFBQSxBQUNDO0FBREQ7QUFBQSxFQUFBLGtCQUNDLEFBQUM7O2FBQUQ7ZUFBQSxBQUNDO0FBREQ7QUFBQTtPQUNDLEFBQ0ssQUFDSjtRQUZELEFBRU07YUFGTjtlQUZGLEFBQ0MsQUFDQyxBQUtEO0FBTEM7QUFDQyxzQkFJRixBQUFDOzthQUFEO2VBUEQsQUFPQyxBQUNDO0FBREQ7QUFBQSxXQVJGLEFBQ0MsQUFRUSxBQUdUO0FBYkQiLCJmaWxlIjoiTGF5b3V0LmpzIiwic291cmNlUm9vdCI6Ii9Vc2Vycy9yYWNoZWxiaXJkL0NvZGUvRXRoZXJldW0gYW5kIFNvbGlkaXR5IENsYXNzIFByb2plY3RzL2tpY2tzdGFydCJ9
--- pages/index.js
+++ pages/index.js
@@ -26,17 +26,14 @@ class CampaignIndex extends Component {
 		return (
 			<Layout>
 				<div>
-					<link
-						rel="stylesheet"
-						href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.3/semantic.min.css">
-					</link>
 					<h3>Open Campaigns</h3>
-					{this.renderCampaigns()}
 					<Button
+						floated="right"
 						content="Create Campaign"
 						icon="add circle"
 						primary
 					/>
+					{this.renderCampaigns()}
 				</div>
 			</Layout>
 		)
